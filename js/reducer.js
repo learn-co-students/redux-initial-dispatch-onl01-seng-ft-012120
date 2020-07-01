7c4fa7c4a2fd4489afb49c2014ec911cb4f6c283
xdundun <xdundun@gmail.com>
2020-07-01 14:23:32 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,27 +1,22 @@
 let state;
-function reducer(state = { count: 0 }, action) {
+
+function changeState(state = { count: 0 }, action) {
   switch (action.type) {
     case "INCREASE_COUNT":
       return { count: state.count + 1 };
+
     default:
       return state;
   }
 }
 
 function dispatch(action) {
-  state = reducer(state, action);
+  state = changeState(state, action);
   render();
 }
 
 function render() {
-  let container = document.getElementById("container");
-  container.textContent = state.count;
+  document.body.textContent = state.count;
 }
 
 dispatch({ type: "@@INIT" });
-
-let button = document.getElementById("button");
-
-button.addEventListener("click", () => {
-  dispatch({ type: "INCREASE_COUNT" });
-});
